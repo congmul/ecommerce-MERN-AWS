fda66c8ff73b767b8b4b2e02488b9fcd5ee61453
Jehyun <congmul@hotmail.com>
2021-04-22 21:21:33 -0700

    Posting-product form

--- client/src/components/PostProduct/index.js
+++ client/src/components/PostProduct/index.js
@@ -19,28 +19,39 @@ function PostProduct() {
 
     return (
         <div className="container-post-product">
-            <div className="input-form">
-            <label className="label-left" for="category">Choose a category : </label>
-            <select value='Category' name="category" onChange={onChange}>
-                <option value='women'>WOMEN</option>
-                <option value='men'>MEN</option>
-                <option value='kids'>KIDS</option>
-            </select>
+            <div className="posting-header">Listing details</div>
+            <div className="posting-left-menu">
+                {/* <div className="subHead">Listing details</div> */}
+                <div>CATEGORY </div>
+                <div>TITLE </div>
+                <div>PRICE </div>
+                <div>IMAGES </div>
             </div>
-            <div className="input-form">
-            <label className="label-left">Title : </label>
-            <input type="text" name="title" onChange={onChange} />
+            <div className="posting-content">
+                <div className="input-form">
+                {/* <label className="label-left" for="category">Choose a category : </label> */}
+                <select value='Category' name="category" onChange={onChange}>
+                    <option value='women'>WOMEN</option>
+                    <option value='men'>MEN</option>
+                    <option value='kids'>KIDS</option>
+                </select>
+                </div>
+                <div className="input-form">
+                {/* <label className="label-left">Title : </label> */}
+                <input type="text" name="title" onChange={onChange} />
+                </div>
+                <div className="input-form">
+                {/* <label className="label-left">Price : </label> */}
+                <input type="text" name="price" onChange={onChange} />
+                </div>
+                <div className="input-form">
+                    {/* <label className="label-left">Images : </label> */}
+                    <input id="" type="file" name="files" multiple />
+                </div>
+                <button onClick={postBtn}>Post the Product</button>
+                <button className="cancel-button">Cancel</button>
             </div>
-            <div className="input-form">
-            <label className="label-left">Price : </label>
-            <input type="text" name="price" onChange={onChange} />
-            </div>
-            <div className="input-form">
-                <label className="label-left">Images : </label>
-                <input id="" type="file" name="files" multiple />
-                <img id="" src="" />
-            </div>
-            <button className="" onClick={postBtn}>Post the Product</button>
+            
         </div>
     )
 }
