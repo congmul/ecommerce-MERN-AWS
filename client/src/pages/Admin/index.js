39b8acbc028f7c7c831b95dd83222f1e40ed631e
Jehyun <congmul@hotmail.com>
2021-05-03 22:29:44 -0700

    Add Nav for Post Orders on Admin page

--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PostProduct from '../../components/PostProduct'
+import Orders from '../../components/Orders'
+import './style.scss';
 
 function Admin() {
+
+    const [rightMenuToggle, setRightMenuToggle] = useState("post");
+
+    const onClick = (e) =>{
+        console.log(e.target.dataset.name);
+        setRightMenuToggle(e.target.dataset.name);
+    }
+
+
     return (
-        <div>
-            <PostProduct />
+        <div className="container-admin">
+            <div className="admin-left">
+                <div className="admin-left-menu" data-name="post" onClick={onClick}>Post</div>
+                <div className="admin-left-menu" data-name="orders" onClick={onClick}>Orders</div>
+            </div>
+            <div className="admin-right">
+                {rightMenuToggle === "post" ? (<PostProduct />):(<Orders />)}
+                
+            </div>
         </div>
     )
 }
